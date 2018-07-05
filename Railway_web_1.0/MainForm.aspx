7cfa9458d0840499300c08d346062f853f1834b5
mrlead <krendelek1509999@gmail.com>
2018-07-05 23:20:24 +0300

    Начинаю портировать код с опытного образца программы.

---
+++ Railway_web_1.0/MainForm.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainForm.aspx.cs" Inherits="Railway_web_1._0.MainForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="MainForm" runat="server">
+        <div style="height: 299px">
+        </div>
+    </form>
+</body>
+</html>
---
+++ Railway_web_1.0/MainForm.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Railway_web_1._0
+{
+    public partial class MainForm : System.Web.UI.Page
+    {
+        class Train
+        {
+            public List<string> num = new List<string>();
+            public List<string> unum = new List<string>();
+            public List<string> station = new List<string>();
+            public List<string> from_time = new List<string>();
+            public List<string> to_time = new List<string>();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
